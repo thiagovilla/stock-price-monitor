356d4152f20383cca564a03dbc2bbc0384146789
Thiago Villa <thiagovilla89@gmail.com>
2020-12-19 11:18:14 -0300

    Rename Asset property 'name' to 'symbol'

--- assets/models.py
+++ assets/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Asset(models.Model):
     """A tracked asset with lower and upper price limits"""
-    name = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=200)
     lower_limit = models.FloatField()
     upper_limit = models.FloatField()
 
     def __str__(self):
         """String for representing the Asset object (e.g. in Admin site)."""
-        return self.name
+        return self.symbol
